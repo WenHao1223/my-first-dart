92876bcb469ff50760a569d589227412c6f370db
Lim Wen Hao <wenhaojshs@gmail.com>
2024-08-31 21:31:19 +0800

    Lesson 1: output and setup .vscode/launch.json

---
+++ bin/1_output.dart
@@ -0,0 +1,22 @@
+void main () {
+  print("Hello World!");
+  print("Hi Ali's");
+  print(
+'''
+
+Hello world
+''');
+  print(3+5);
+  print("3 + 5");
+  print("3 + 5 = ${3+5}");
+}
+
+// Output:
+// Hello World!
+// Hi Ali's
+
+// Hello world
+
+// 8
+// 3 + 5
+// 3 + 5 = 8
